6f876523292d66986a6076604600955fabae4ae4
Daniel <1064265199@qq.com>
2022-08-18 23:47:41 +0800

    config

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # MysqlToRedisjson
+
+https://github.com/redis/redis-om-spring
+
+https://github.com/redis/jedis
+
+https://redis.com/modules/redis-json/
+
+https://developer.redis.com/howtos/redisjson/using-java/
+
+https://redis.io/docs/stack/search/indexing_json/
+
+https://redis.io/docs/stack/get-started/install/mac-os/
--- src/main/java/org/mysqltoredisjson/Application.java
+++ src/main/java/org/mysqltoredisjson/Application.java
@@ -1,13 +1,23 @@
 package org.mysqltoredisjson;
 
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @MapperScan("org.mysqltoredisjson.mapper")
 @SpringBootApplication
 public class Application {
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    CommandLineRunner loadTestData() {
+        return args -> {
+
+        };
     }
 }
